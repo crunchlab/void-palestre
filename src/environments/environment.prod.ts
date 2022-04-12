01121f029489ee0b4baffa0885d571437a00a536
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-04-12 10:33:42 +0200

    updated about

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,25 @@
-import featureToPalestraMappings from "src/mappings/featureToStrutturaMappings";
 import filtersFieldMappings from "../mappings/filtersFieldMappings";
+import featureToPalestraMappings from "../mappings/featureToStrutturaMappings";
 
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 export const environment = {
-    production: true,
+    production: false,
     fieldMappings: featureToPalestraMappings,
     filtersFieldMappings: filtersFieldMappings,
     mapStyle: './assets/map-styles/roadmap-style.json',
     dataPointColorMap: './assets/map-styles/data-points-colors.json',
-    version: "1.0.2-beta"
+    version: "0.0.1-alpha"
+    // mapStyle: './assets/map-styles/dark-style.json'
 
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
